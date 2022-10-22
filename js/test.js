7187fbf92b3ccd4930a0f8c57b15bfc4040182e7
Ray Chung <raychungno1@gmail.com>
2022-10-22 12:18:56 -0400

    initial test

---
+++ js/test.js
@@ -0,0 +1,20 @@
+//@include utils.js;
+//@include cards.js;
+var proj = app.project;
+// $.write(5);
+// CREATE A FOLDER
+var bin1 = proj.rootItem.createBin("Sequence1");
+// alert(bin1.name);
+// RENAME FOLDER
+bin1.name = "Movies";
+// MOVE CLIP FOLDER INTO MOVIES FOLDER
+var moveThis = proj.rootItem.createBin("Inner");
+var toHere = proj.rootItem.createBin("Outer");
+moveThis.moveBin(toHere);
+// CREATE A SEQUENCE
+var seq = proj.createNewSequenceFromClips("Clips Sequence", [proj.rootItem.children[10], proj.rootItem.children[11]], proj.rootItem);
+// MOVE INTO FOLDER
+var excludes = ["TRANSITIONPUNCH_5.0", "SFX"];
+var targetSeq = find("Clips Sequence", proj.rootItem, excludes);
+var targetBin = find("Inner", proj.rootItem, excludes);
+targetSeq.moveBin(targetBin);
